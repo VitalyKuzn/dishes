14d69dbaba369cf7f2b6be61dd965bd14cf0c6b3
Кузнецов Виталий <teriki2017@yandex.ru>
2022-02-23 11:00:03 +0500

    Dishes

--- dishes.py
+++ dishes.py
@@ -1,37 +1,46 @@
 from pprint import pprint
 
-def create_cook_book(dishes_text):
+
+def create_cook_book(input_file):
     cook_book = {}
 
-    with open(dishes_text, encoding='utf-8') as f:
+    with open(input_file, encoding='utf-8') as f:
         lst = [line.strip() for line in f]
 
+    for i, c in enumerate(lst):
+        
+        if c.isdigit():
+            # если элемент - цифра, то берем предыдущий элемент
+            cook_book[lst[i-1]] = []
+            
+            for slice in lst[i+1:i+int(c)+1]:
+                ingredient_name = slice.split('|')[0]
+                quantity = int(slice.split('|')[1])
+                measure = slice.split('|')[2]
 
-        for i, c in enumerate(lst):
-            if c.isdigit():
-                cook_book[lst[i - 1]] = []
-
-                for slice in lst[i+1:i+int(c)+1]:
-                    ingredient_name = slice.split('| ' )[0]
-                    quantity = int(slice.split('| ')[1])
-                    measure = slice.split('| ')[2]
-
-                    cook_book[lst[i - 1]].append({'ingredient_name':ingredient_name, 'quantity':quantity, 'measure':measure})
+                cook_book[lst[i-1]].append({'ingredient_name':ingredient_name, 'quantity':quantity, 'measure':measure})
+    
     return cook_book
 
-def get_shop_list_by_dishes(dishes, cooking_book, person_count):
-    cook_book = cooking_book
-    for key in dishes:
-        try:
-            for value in cook_book[key]:
-                value['quantity'] *= person_count
-                print(value)
-        except:
-            pass
 
-print('Задача №1:\n')
+def get_shop_list_by_dishes(dishes, person_count, cooking_book):
+    ing_dict = {}
+
+    for key in cooking_book.keys ():
+        for dish in dishes:
+            if key == dish:
+                for dictionary in cooking_book[key]:
+                    ing_name = dictionary['ingredient_name']
+
+                    try:
+                        ing_dict[ing_name]['quantity'] += (dictionary['quantity'] * person_count)
+                    except:
+                        ing_dict[ing_name] = {'measure': dictionary['measure'], 'quantity': dictionary['quantity'] * person_count}
+
+    return ing_dict
+
+print('Задание №1:\n')
 pprint(create_cook_book('dishes_text.txt'))
-
-print('Задача №2:\n')
-get_shop_list_by_dishes(['Омлет', 'Запеченный картофель','Фахитос'],\
-                        create_cook_book('dishes_text.txt'), 15) 
+print('\n' * 3)
+print('Задание №2:\n')
+pprint(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Фахитос'], 15, create_cook_book('dishes_text.txt')))
